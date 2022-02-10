4c2194b0f829cf4e695d324e9d5f27afad15d402
Leo Cormoreche <leocormo@gmail.com>
2022-02-10 17:24:31 +0100

    commit after timer

--- src/index.js
+++ src/index.js
@@ -6,11 +6,67 @@ const bodyDiv = document.body
 // Creation d'une div
 const startDiv = document.createElement('div');
 
+// Creation du timer avec la balise h
+const timer = document.createElement('h3');
+timer.id = 'timer';
 
+// Creation du boutton
 const startButton = document.createElement('button');
 startButton.innerText = 'commencer';
 
 // Envoie div dans app
 app.append(startDiv);
 
+<<<<<<< HEAD
 startDiv.append(startButton);
+=======
+startDiv.append(startButton);
+
+startDiv.append(timer);
+
+
+/** COMPTE A REBOURS **/
+
+
+
+const addMinutes =  function (dt, minutes) {
+    return new Date(dt.getTime() + (minutes*60000)+2000);
+}
+
+
+
+
+const startTimer = () => {
+    let currentDate = new Date();
+    let newDate = addMinutes(currentDate, 15);
+        setInterval(function () {
+
+        let now = new Date().getTime();
+
+
+        let distance = newDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        timer.innerText = minutes +":"+ seconds;
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval();
+            timer.innerText = "EXPIRED";
+        }
+    }, 1000);
+}
+
+
+
+
+startButton.addEventListener('click', () => {
+    startButton.remove();
+    startTimer()
+})
+>>>>>>> ab966a1 (JS pret pour timer)
