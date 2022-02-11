f3f0c6b975242a780cd7766c3755c5b5acf7d03f
Leo Cormoreche <leocormo@gmail.com>
2022-02-11 12:03:38 +0100

    commit discord

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ const app = document.getElementById('app');
 
 const bodyDiv = document.body
 
-// Creation d'une div
+// Creation de la div de depart
 const startDiv = document.createElement('div');
+
+// Creation de la div left
+const splitLeft = document.createElement('div');
+splitLeft.id = 'split left';
+
+// Creation de la div right
+const splitRight = document.createElement('div');
+splitRight.id = 'split right';
 
 // Creation du timer avec la balise h
 const timer = document.createElement('h3');
@@ -14,21 +22,22 @@ timer.id = 'timer';
 //const txt = document.createElement('p');
 
 const iframe = document.createElement('div');
-iframe.innerHTML = '<widgetbot server="941334066718990346" channel="941334067163582485" width="800" height="600"></widgetbot>'
+iframe.innerHTML = '<widgetbot server="941334066718990346" channel="941334067163582485" width="800" height="600"></widgetbot>';
 iframe.style.display = 'none';
 
 // Creation du boutton
 const startButton = document.createElement('button');
+startButton.id = 'startButton';
 startButton.innerText = 'commencer';
 
 // Envoie div dans app
 app.append(startDiv);
 
 startDiv.append(startButton);
-
-startDiv.append(timer);
-
-startDiv.append(iframe);
+app.append(splitLeft);
+app.append(splitRight);
+splitRight.append(timer);
+splitLeft.append(iframe);
 
 
 /** COMPTE A REBOURS **/
@@ -58,13 +67,27 @@ const startTimer = () => {
         // If the count down is over, write some text
         if (distance < 0) {
             clearInterval();
-            timer.innerText = "EXPIRED";
+            timer.innerText = "GAME OVER";
         }
     }, 1000);
 }
 
 startButton.addEventListener('click', () => {
     iframe.style.display = 'contents';
+    startDiv.remove();
     startButton.remove();
-    startTimer()
+
+    // Creation de l'input
+    const input = document.createElement('input');
+    input.id='input';
+
+    // Creation du boutton
+    const submitButton = document.createElement('button');
+    submitButton.id = 'submitButton';
+    submitButton.innerText = 'Valider';
+
+
+    splitRight.append(input);
+    splitRight.append(submitButton);
+    startTimer();
 })
